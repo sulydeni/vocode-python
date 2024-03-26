0f9016dff0968ecce3b589557abab5ea5f2a00fc
Suleiman Deni <suleiman@eyekandy.com>
2024-03-26 23:36:49 +0000

    use client backend customConversation

--- apps/client_backend/main.py
+++ apps/client_backend/main.py
@@ -1,36 +1,59 @@
 import logging
+import os
 from fastapi import FastAPI
 
-from vocode.streaming.models.agent import ChatGPTAgentConfig
-from vocode.streaming.models.synthesizer import AzureSynthesizerConfig
+from vocode.streaming.agent.restful_user_implemented_agent import RESTfulUserImplementedAgent
+from vocode.streaming.models.agent import ChatGPTAgentConfig, RESTfulUserImplementedAgentConfig
+from vocode.streaming.models.synthesizer import AzureSynthesizerConfig, ElevenLabsSynthesizerConfig
+from vocode.streaming.models.transcriber import AssemblyAITranscriberConfig
 from vocode.streaming.synthesizer.azure_synthesizer import AzureSynthesizer
 
 from vocode.streaming.agent.chat_gpt_agent import ChatGPTAgent
-from vocode.streaming.client_backend.conversation import ConversationRouter
+from customConversation import ConversationRouter
 from vocode.streaming.models.message import BaseMessage
 
 from dotenv import load_dotenv
 
+from vocode.streaming.synthesizer.eleven_labs_synthesizer import ElevenLabsSynthesizer
+from vocode.streaming.transcriber.assembly_ai_transcriber import AssemblyAITranscriber
+
 load_dotenv()
 
-app = FastAPI(docs_url=None)
+app = FastAPI()
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+rest_lifex_call= RESTfulUserImplementedAgent(
+    RESTfulUserImplementedAgentConfig(
+        initial_message=BaseMessage(text="How are you doing you?"),
+        respond=RESTfulUserImplementedAgentConfig.EndpointConfig(
+            url="https://api-v1.yourlifex.com/api/v1/response_api/fd69a44d-d0e9-45f9-a948-30c270e7bacc/",
+            method="POST"
+        ),
+        generate_responses=False,
+    ),
+    logger=logger
+    )
+
 conversation_router = ConversationRouter(
-    agent_thunk=lambda: ChatGPTAgent(
-        ChatGPTAgentConfig(
-            initial_message=BaseMessage(text="Hello!"),
-            prompt_preamble="Have a pleasant conversation about life",
-        )
-    ),
-    synthesizer_thunk=lambda output_audio_config: AzureSynthesizer(
-        AzureSynthesizerConfig.from_output_audio_config(
-            output_audio_config, voice_name="en-US-SteffanNeural"
-        )
-    ),
+    agent_thunk=lambda: rest_lifex_call,
+    transcriber_thunk= lambda input_audio_config: AssemblyAITranscriber(
+            AssemblyAITranscriberConfig.from_input_audio_config(
+                input_audio_config=input_audio_config,
+                api_key=os.getenv("ASSEMBLY_AI_API_KEY"),
+            )
+        ),
+        synthesizer_thunk= lambda output_audio_config: ElevenLabsSynthesizer(
+            ElevenLabsSynthesizerConfig.from_output_audio_config(
+                output_audio_config=output_audio_config,
+                api_key=os.getenv("ELEVENLABS_API_KEY"),
+                voice_id=os.getenv("ELEVENLABS_VOICE_ID"),
+                stability=1.0,
+                similarity_boost=1.0,
+            ),
+        ),
     logger=logger,
 )
 
